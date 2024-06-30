01192f0f9b0d61cea21d82962f5caf810f06fd78
MateusToni <mateus.toni@buttinimoraes.com.br>
2024-06-30 19:26:09 -0300

    feat(mario): init_project

--- scripts/mario.gd
+++ scripts/mario.gd
@@ -1,13 +1,36 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func decide_action(direction):
+	
+	if direction == -1:
+		
+		animated_sprite.flip_h = false
+		if is_on_floor():
+			animated_sprite.play("walk")
+		
+	elif direction == 1:
+		
+		animated_sprite.flip_h = true
+		if is_on_floor():
+			animated_sprite.play("walk")
+		
+	elif direction == 0:
+		
+		animated_sprite.play("idle")
+		
+	if not is_on_floor():
+		
+		animated_sprite.play("jump")
+	
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -20,6 +43,9 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	decide_action(direction)
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
